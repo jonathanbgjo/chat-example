2817335a1a50884290d946cd7678400e87ee7932
Jonathan Jo <Jonathanbgjo@gmail.com>
2019-12-16 13:39:21 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+}) 
+io.on('connection', function(socket){
+    console.log('a user connected');
+})
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
+  });
+  io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+  });
+http.listen(8000, function(){
+    console.log('listening on *:8000');
+})
+
